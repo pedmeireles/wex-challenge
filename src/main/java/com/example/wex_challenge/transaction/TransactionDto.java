c518107366259328d0defd37ff122e0254dfa840
Pedro Meireles <pedmeireles@gmail.com>
2025-07-15 20:09:47 -0300

    final tune ups

--- src/main/java/com/example/wex_challenge/transaction/TransactionDto.java
+++ src/main/java/com/example/wex_challenge/transaction/TransactionDto.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.PastOrPresent;
